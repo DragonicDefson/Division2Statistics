4cc96cc3c1d9976e61a9acb7da5c81749169383e
Bradley Methorst <37877161+DragonicDefson@users.noreply.github.com>
2023-02-24 01:28:42 +0100

    Updated initial files.
    Minor changes, Windows installer fix.

--- main.js
+++ main.js
@@ -1,74 +1,70 @@
 const { app, BrowserWindow } = require('electron')
-const cfg = require('./config.json')
+const config = require('./config.json')
 const { ElectronBlocker } = require('@cliqz/adblocker-electron')
 const fetch = require('cross-fetch')
 const fs = require('fs')
 const http = require('http')
+const resources = process.resourcesPath
 
 app.disableHardwareAcceleration()
 
-let win = undefined
-
 const createWindow = () => {
-  let iconExt = undefined
-  if (process.platform !== 'darwin') {
-    iconExt = 'icon.ico'
+  let iconExt = undefined; let platform = process.platform
+  switch (platform) {
+    case 'linux': iconExt = 'icon.png'; break;
+    case 'darwin': iconExt = 'icon.icns'; break;
+    default: iconExt = 'icon.ico'; break;
   }
-  win = new BrowserWindow({
+  const win = new BrowserWindow({
     show: false,
-    icon: `${process.resourcesPath}/appicons/${iconExt}`,
+    icon: `${resources}/appicons/${iconExt}`,
     webPreferences: {
       nodeIntegration: false
     }
   })
-  win.maximize()
-  const session = win.webContents.session
-  if (cfg.application.debug_enabled) {
-    loadApp(win)
-    fileLoader(session)
-    win.webContents.openDevTools()
-  } else {
-    loadApp(win)
-    fileLoader(session)
+  const session = win.webContents.session; win.maximize()
+  switch(config.application.debug_enabled) {
+    case true:
+      loadApp(win)
+      fileLoader(session)
+      win.webContents.openDevTools()
+    break;
+    default:
+      loadApp(win)
+      fileLoader(session)
+    break;
   }
 }
 
 const fileLoader = (session) => {
-  const webserver_port = cfg.application.webserver_port
+  const webserver_port = config.application.webserver_port
   http.createServer((request, response) => {
-    const pathName = request.url
-    fs.readFile(`${process.resourcesPath}/adlists${pathName}`, (exception, data) => {
-      if (exception) { return } else {
-        response.end(data)
-      }
+    let pathname = request.url
+    fs.readFile(`${resources}/adlists${pathname}`, (exception, data) => {
+      if (exception) { return; } else { response.end(data) }
     })
-  }).listen(webserver_port)
+  }).listen(webserver_port, 'localhost')
   ElectronBlocker.fromLists(fetch, [
     `http://localhost:${webserver_port}/easylist.txt`,
     `http://localhost:${webserver_port}/ultimate-ad-filter.txt`
   ]).then((blocker) => {
-    if (cfg.application.adblock_enabled) {
-      blocker.enableBlockingInSession(session)
-    } else {
-      blocker.disableBlockingInSession(session)
+    switch (config.application.adblock_enabled) {
+      case true: blocker.enableBlockingInSession(session); break;
+      default: blocker.disableBlockingInSession(session); break;
     }
   })
 }
 
 const loadApp = (win) => {
-  const username = cfg.application.username
-  win.loadURL(`${cfg.application.url}${username}/overview`).then(() => {
-    win.setTitle(`${username} - Division 2 - statistics`)
+  const username = config.application.username
+  win.loadURL(`${config.application.url}${username}/overview`).then(() => {
+    win.setTitle(`${username} - The Division 2 - statistics`)
   })
   win.show()
 }
 
 app.whenReady().then(() => {
-  if (require('electron-squirrel-startup')) { app.quit() }
-  createWindow()
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) { createWindow() }
-  })
+  if (require('electron-squirrel-startup')) { app.quit() } else { createWindow() }
   app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') { app.quit() }
   })
